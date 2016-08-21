4475c476e98216fbe1d48f11ceffb893b25f9a4a
alvin <xiaozhuang.dong@ele.me>
2016-08-21 18:44:31 +0800

    aa

---
+++ src/main/java/com/alvin/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.alvin;
+
+/**
+ * Created by alvin on 16/8/21.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello,world!");
+    }
+}
